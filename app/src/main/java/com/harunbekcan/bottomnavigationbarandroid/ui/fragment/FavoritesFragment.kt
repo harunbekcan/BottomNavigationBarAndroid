209a12b5838a78a82e936e16e1ba92dd1d62569c
Harun Bekcan <harun.bekcan@applogist.com>
2022-08-05 23:38:13 +0300

    favorites and profilePage clicked completed

--- app/src/main/java/com/harunbekcan/bottomnavigationbarandroid/ui/fragment/FavoritesFragment.kt
+++ app/src/main/java/com/harunbekcan/bottomnavigationbarandroid/ui/fragment/FavoritesFragment.kt
@@ -10,4 +10,5 @@ class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>() {
     override fun getLayoutId(): Int = R.layout.fragment_favorites
 
     override fun prepareView(savedInstanceState: Bundle?) {}
+
 }
--- app/src/main/java/com/harunbekcan/bottomnavigationbarandroid/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/harunbekcan/bottomnavigationbarandroid/ui/fragment/HomeFragment.kt
@@ -1,13 +1,27 @@
 package com.harunbekcan.bottomnavigationbarandroid.ui.fragment
 
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.harunbekcan.bottomnavigationbarandroid.R
 import com.harunbekcan.bottomnavigationbarandroid.base.BaseFragment
 import com.harunbekcan.bottomnavigationbarandroid.databinding.FragmentHomeBinding
+import com.harunbekcan.bottomnavigationbarandroid.ui.activity.MainActivity
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun getLayoutId(): Int = R.layout.fragment_home
 
-    override fun prepareView(savedInstanceState: Bundle?) {}
+    override fun prepareView(savedInstanceState: Bundle?) {
+        initListener()
+    }
+
+    private fun initListener(){
+        binding.goToFavoritesPageButton.setOnClickListener {
+            (requireActivity() as? MainActivity)?.findViewById<BottomNavigationView>(R.id.bottomNavigation)?.selectedItemId =
+                R.id.nav_favorite
+        }
+        binding.goToProfilePageButton.setOnClickListener {
+            (requireActivity() as? MainActivity)?.findViewById<BottomNavigationView>(R.id.bottomNavigation)?.selectedItemId = R.id.nav_profile
+        }
+    }
 }
